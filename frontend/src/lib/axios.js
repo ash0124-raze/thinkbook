eb53c38dcee4c3af809c74a494d5c131a6cdc783
Ashmit verma <ashmitverma3037@gmail.com>
2026-08-12 01:25:15 +0530

    axios fix

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://thinkbook-leaz.onrender.com";
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "https://thinkbook-leaz.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
